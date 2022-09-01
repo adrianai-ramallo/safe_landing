923e8a2fea443152c40a7d0ebfeb49f4467e6c99
adriana-ramallo <adriana.ramallo.ines@gmail.com>
2022-09-01 20:34:24 +0100

    Version 11-  Final Graphs enhancements and fixes to win/ game over features after testing

--- main.py
+++ main.py
@@ -1,17 +1,13 @@
 #To be able to run this program: 1.Go to Python Packages 2.Search for 'kivy' 3.Install
-#flappy bird https://www.youtube.com/watch?v=cGYMZB_peBM&list=PLy5hjmUzdc0mSEN7WxUQ_HlP6X_OdvMlq
+
 
 from kivy.app import App
 from kivy.uix.widget import Widget
 from kivy.properties import ObjectProperty
 from kivy.uix.image import Image
 from kivy.core.window import Window
-from kivy.graphics import Color, Rectangle
 from random import randint
 from kivy.properties import NumericProperty
-
-from kivy.uix.screenmanager import ScreenManager, Screen
-
 
 
 class Check(Widget):
@@ -54,16 +50,6 @@ class Pines(Image):
     velocity = NumericProperty(0)
     pines= ObjectProperty()
 
-   # def on_touch_down(self, touch):
-   #     self.source = "pines.png"
-   #     self.velocity = 150
-   #     super().on_touch_down(touch)
-
-   # def on_touch_up(self, touch):
-   #     self.source = "volcane_low.png"
-    #    #self.velocity = -80
-    #    super().on_touch_up(touch)
-
 #Win Points
 class Purple_cloud(Image):
     #velocity = NumericProperty(0)
@@ -81,7 +67,7 @@ class Purple_cloud(Image):
 
 class Bor_cloud(Image):
     #velocity = NumericProperty(0)
-    bor_cloud= ObjectProperty()
+    bor_cloud = ObjectProperty()
 
     def on_touch_down(self, touch):
         self.source = "red_cloud2.png"
@@ -109,7 +95,7 @@ class Yell_cloud(Image):
 
 class Background(Widget):
     grass_texture = ObjectProperty()
-  #  pines_texture = ObjectProperty()
+    sky_texture = ObjectProperty()
     red_texture = ObjectProperty(None)
     clouds_texture = ObjectProperty(None)
     water_texture = ObjectProperty(None)
@@ -120,12 +106,6 @@ class Background(Widget):
         #self.grass_texture = Image(source="pines.png").texture
         self.grass_texture.wrap = "repeat"
         self.grass_texture.uvsize = (Window.width/self.grass_texture.width*2,-1)
-
-   #     self.pines_texture = Image(source="pines.png").texture
-       # self.pines_texture.uvsize = (80, 80)
-     #   self.pines_texture.wrap = "repeat"
-       # self.pines_texture.wrap = ""
-   #     self.pines_texture.uvsize = (Window.width/180,-1)
 
         self.red_texture = Image(source="red.png").texture
         self.red_texture.wrap ="repeat"
@@ -147,24 +127,10 @@ class Background(Widget):
         texture.dispatch(self)
     pass
 
-  #  def scroll_pines(self, time):
-        #pines
-  #      self.pines_texture.uvpos = (((self.pines_texture.uvpos[0]+ time/4)%Window.height),(self.pines_texture.uvpos[1]+1))
-   #     texture = self.property("pines_texture")
-  #      texture.dispatch(self)
-  #  pass
-
     def scroll_red(self, time):
         #pines
         self.red_texture.uvpos = (((self.red_texture.uvpos[0]+ time/10)%Window.height),(self.red_texture.uvpos[1]+1))
         texture = self.property("red_texture")
-        texture.dispatch(self)
-    pass
-
-    def scroll_water(self, time):
-        #water
-        self.water_texture.uvpos = (((self.water_texture.uvpos[0]+ time/6)%Window.height),(self.water_texture.uvpos[1]+1))
-        texture = self.property("water_texture")
         texture.dispatch(self)
     pass
 
@@ -179,31 +145,28 @@ class Background(Widget):
 from kivy.clock import Clock
 
 class MainApp(App):
-    #volcano = []
     GRAVITY = 100
     WIND = 10
     score = NumericProperty(0)
     life = NumericProperty(3)
 
-    #def next_frame(self):
-     #   Clock.schedule_interval(self.root.ids.background.scroll_grass, 1/60)
-      #  Clock.schedule_interval(self.root.ids.background.scroll_pines, 1 / 60)
-       # Clock.schedule_interval(self.root.ids.background.scroll_red, 1 / 60)
-        #Clock.schedule_interval(self.root.ids.background.scroll_water, 1 / 60)
-        #Clock.schedule_interval(self.root.ids.background.scroll_clouds, 1 / 60)
-
     def move_volcano(self, time):
         volcano = self.root.ids.volcano
         volcano.x = volcano.x - randint(1,2)
+
         if volcano.x <= -100:
-            volcano.x = Window.width+20
-            volcano.x = volcano.x - randint(0, 60)
+            volcano.x = Window.width + randint(1,10)
+
+
+
     def move_pines(self, time):
         pines = self.root.ids.pines
         pines.x = pines.x - randint(1,2)
         if pines.x <= -100:
             pines.x = Window.width+20
-            pines.x = pines.x - randint(0, 40)
+
+
+         #   pines.x = pines.x - randint(0, 40)
     def move_purple_cloud(self, time):
         purple_cloud = self.root.ids.purple_cloud
         purple_cloud.x = purple_cloud.x - 0.7
@@ -232,27 +195,37 @@ class MainApp(App):
         grass = self.root.ids.background.grass_texture
         pines = self.root.ids.pines
         volcano = self.root.ids.volcano
-        #volcano.x = volcano.x - 0.7
-
         purple_cloud = self.root.ids.purple_cloud
-
         yell_cloud = self.root.ids.yell_cloud
         bor_cloud = self.root.ids.bor_cloud
-    # purple_cloud.x = purple_cloud.x - 0.7
-
 
         if balloon.y > grass.height:
             if balloon.y >= Window.height-70:
                 balloon.y = balloon.y - self.GRAVITY * time * 15
-            #else:
-                #for volcano in self.volcano:
+
             elif volcano.collide_widget(balloon) or pines.collide_widget(balloon):
-                balloon.y = 600 + balloon.velocity * time
-                balloon.x = balloon.x +60 + self.WIND * time
-                balloon.velocity = balloon.velocity - self.GRAVITY * time
-                self.life -= 1
-                if self.life == 0:
-                    self.game_over(time)
+
+
+                if self.life == 1:
+                    self.frames.cancel()
+                    self.life = 0
+                    balloon.size = (350, 350)
+                    balloon.pos = (200, 200)
+                    balloon.size_hint= (None, None)
+                    balloon.source = "heart.png"
+
+                    label = self.root.ids.label
+                    label.text = "Oh no! \nGame Over!"
+                    label.pos = (50, 290)
+                    label.size = (160, 150)
+                    label.color = (179, 250, 22, 1)
+
+                elif self.life >1:
+                    self.life -= 1
+                    balloon.y = 600 + balloon.velocity * time
+                    balloon.x = balloon.x + self.WIND * time
+                    balloon.velocity = balloon.velocity - self.GRAVITY * time*20
+
 
             elif purple_cloud.collide_widget(balloon):
                 balloon.y = balloon.y -20 + balloon.velocity * time
@@ -281,57 +254,16 @@ class MainApp(App):
                 balloon.velocity = balloon.velocity - self.GRAVITY * time
 
         elif balloon.y <= grass.height:
-            #balloon.y = 0
-            #balloon.x = 0
-            #balloon.velocity = 0
-            balloon.size = (450,450)
-            balloon.pos=(50,50)
+            balloon.size = (350,350)
+            balloon.pos=(200,200)
             self.frames.cancel()
-            pass
+            label = self.root.ids.label
+            label.text = "Safe Landing! \nScore " + str(self.score)
+            label.pos=(50,250)
+            label.size = (160,150)
+            label.color = (179,250,22,1)
+    pass
 
-
-    def game_over(self, time):
-        balloon = self.root.ids.balloon
-        #if self.life == 0:
-      #  balloon.y == 250
-        balloon.y = 400
-        #balloon.x = 400
-        balloon.velocity = 0
-        balloon.source = "heart.png"
-        balloon.size = (450, 450)
-        balloon.pos = (50, 50)
-        if balloon.y <= 400:
-            self.frames.cancel()
-
-       #     balloon.y = balloon.y + balloon.velocity * time/2
-        #    balloon.x = balloon.x
-       #     balloon.velocity = 0
-        #    if balloon.y <= 200:
-        #        self.frames.cancel()
-           # self.end()
-        #self.check_collision(time)
-  #  def end(self):
-   #     self.frames.cancel()
-
-   # def score(self, balloon):
-#
-
-    #def won(self, time):
-      #  balloon = self.root.ids.balloon
-       # volcano = self.root.ids.volcano
-    #ask user for a name and save the score
-     #   self.score -= 10
-      #  save_score = input("Do you want to save your score? Y/N")
-       # if save_score =="Y" or save_score == "y":
-        #    name = input("Player name:")
-
-
-   # def on_start(self):
-    #    Clock.schedule_interval(self.root.ids.background.scroll_grass, 1/60)
-
-    #    Clock.schedule_interval(self.root.ids.background.scroll_red, 1 / 60)
-     #   Clock.schedule_interval(self.root.ids.background.scroll_water, 1 / 60)
-      #  Clock.schedule_interval(self.root.ids.background.scroll_clouds, 1 / 60)
 
     def next_frame(self,time_passed):
         self.move_balloon(time_passed)
@@ -343,32 +275,10 @@ class MainApp(App):
         self.root.ids.background.scroll_clouds(time_passed)
         self.root.ids.background.scroll_grass(time_passed)
         self.root.ids.background.scroll_red(time_passed)
-     #   self.root.ids.background.scroll_water(time_passed)
+    pass
 
     def start(self):
         self.frames = Clock.schedule_interval(self.next_frame, 1/60.)
-
-        #num_volcanos = 3
-        #space_btw_volcanos = Window.width/ (num_volcanos -1)
-        #for i in range(num_volcanos):
-         #   volcano = Volcano()
-          #  volcano.size_hint=(None,None)
-           # volcano.pos =(Window.width + i * space_btw_volcanos,70)
-            #volcano.size = (616/2,325/2)
-            #volcano.source = "volcane_low.png"
-
-            #self.volcano.append(volcano)
-            #self.root.add_widget(volcano)
-
-        #def move_volcano(self, time_passed):
-         #   for volcano in self.volcanos:
-          #      volcano.x -= time_passed * 100
-
-           # volcano_xs = list(map(lambda volcano: volcano.x, self.volcanos))
-            #right_most_x = max(volcano.xs)
-            #if right_most_x <= Window.width - space_btw_volcanos:
-             #   most_left_volcano = self.volcanos[volcano_xs.index(min(volcano_xs))]
-              #  most_left_volcano = Window.width
 
     pass
 
